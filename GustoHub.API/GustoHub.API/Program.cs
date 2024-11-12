9838611f68bab3066f2e6cdab36e40aee0284455
Mustafa Buhov <mustyx900@gmail.com>
2024-11-12 23:47:11 +0200

    Resolved merge conflicts in ServiceCollectionExtension.cs and Program.cs

--- GustoHub.API/GustoHub.API/Program.cs
+++ GustoHub.API/GustoHub.API/Program.cs
@@ -12,12 +12,16 @@ namespace GustoHub.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddApplicationDbContext(builder.Configuration);
+            builder.Services.AddApplicationServices();
+
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant API v1"));
             }
 
             app.UseHttpsRedirection();
